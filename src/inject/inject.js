20848d447e5f246f8c8be3a225ed835f51b96d44
Omayeli Arenyeka <aomayeli@gmail.com>
2018-02-27 00:07:46 -0500

    Replace clipboard data with body text

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,33 +1,25 @@
-(function() {
-	var loc = document.location;
-	var uri = {
-	  spec: loc.href,
-	  host: loc.host,
-	  prePath: loc.protocol + "//" + loc.host,
-	  scheme: loc.protocol.substr(0, loc.protocol.indexOf(":")),
-	  pathBase: loc.protocol + "//" + loc.host + loc.pathname.substr(0, loc.pathname.lastIndexOf("/") + 1)
-	};
-	var documentClone = document.cloneNode(true); 
-	var article = new Readability(uri, documentClone).parse();
-	debugger;
+window.onload = function() {
+	(function() {
+		var loc = document.location;
+		var uri = {
+		  spec: loc.href,
+		  host: loc.host,
+		  prePath: loc.protocol + "//" + loc.host,
+		  scheme: loc.protocol.substr(0, loc.protocol.indexOf(":")),
+		  pathBase: loc.protocol + "//" + loc.host + loc.pathname.substr(0, loc.pathname.lastIndexOf("/") + 1)
+		};
+		var documentClone = document.cloneNode(true); 
+		var article = new Readability(uri, documentClone).parse();
 
-	function getAllText() {
+		console.log(SummaryTool.summarizeFromUrl('https://www.newyorker.com/news/daily-comment/four-truths-about-the-florida-school-shooting'));
 
-	}
+		function getAllText() {
+			return (article.content).replace(/(<([^>]+)>)/ig,"");
+		}
 
-	function getContext() {
-
-	}
-
-	function getPageSummary() {
-
-	}
-
-	document.addEventListener('copy', function(e) {
-	  var textToPutOnClipboard = (article.content).textContent;
-	  debugger;
-	  console.log(textToPutOnClipboard, article.content.replace(/(<([^>]+)>)/ig,""));
-	  e.clipboardData.setData('text/plain', textToPutOnClipboard);
-	  e.preventDefault();
-	});
-})();
+		document.addEventListener('copy', function(e) {
+			e.clipboardData.setData('text/plain', getAllText());
+			e.preventDefault();
+		});
+	})();
+};
